1012f94c5091621135f1e190719b0218b02c4785
Daniel Han <hex0cter@gmail.com>
2018-10-08 22:30:56 +0200

    Simply working

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { width: 0, height: 0 };
+    this.state = { sessionId: 'local-notes' /*Date.now()*/, text: '', width: 0, height: 0 };
     this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    this.textUpdateHandler = this.textUpdateHandler.bind(this);
   }
 
   componentDidMount() {
+    /*
+    for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+      console.log( localStorage.key( i ) );
+    }
+    */
+
+    const history = localStorage.getItem('local-notes')
+    if (history) {
+      this.setState({text: history})
+    }
+
     this.updateWindowDimensions();
     window.addEventListener('resize', this.updateWindowDimensions);
   }
@@ -21,11 +33,14 @@ class App extends Component {
     this.setState({ width: window.innerWidth - 10, height: window.innerHeight });
   }
 
-  render() {
-    console.log(`width: ${this.state.width} height ${this.state.height} `)
+  textUpdateHandler(event) {
+    this.setState({text: event.target.value})
+    localStorage.setItem(this.state.sessionId, event.target.value)
+  }
 
+  render() {
     return (
-      <textarea autoFocus style={{width: this.state.width, height: this.state.height}}>
+      <textarea value={this.state.text} autoFocus style={{width: this.state.width, height: this.state.height}} onChange={this.textUpdateHandler}>
       </textarea>
     );
   }
