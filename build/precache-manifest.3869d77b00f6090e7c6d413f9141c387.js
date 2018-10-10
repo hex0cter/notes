686c830bcb7af25809539da69d2577575d9fe1ab
Daniel Han <hex0cter@gmail.com>
2018-10-10 15:13:17 +0200

    Add build to version control

---
+++ build/precache-manifest.3869d77b00f6090e7c6d413f9141c387.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "15a83d65203bfb56a42b",
+    "url": "/static/js/main.15a83d65.chunk.js"
+  },
+  {
+    "revision": "162caccb29d519cebfbe",
+    "url": "/static/js/1.162caccb.chunk.js"
+  },
+  {
+    "revision": "15a83d65203bfb56a42b",
+    "url": "/static/css/main.495c0edf.chunk.css"
+  },
+  {
+    "revision": "7a16e42727a2a087b6f1aa67fbc65af8",
+    "url": "/index.html"
+  }
+];
