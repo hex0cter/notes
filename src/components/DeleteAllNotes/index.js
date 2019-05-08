a151608aebeb825d4f311174bfc7dd9ca77172b4
Daniel Han <hex0cter@gmail.com>
2019-05-08 17:24:19 +0200

    Replace hamburger with ant design icons

--- src/components/DeleteAllNotes/index.js
+++ src/components/DeleteAllNotes/index.js
@@ -2,15 +2,27 @@ import React from 'react'
 import { connect } from "react-redux"
 import mapDispatchToProps from './map-dispatch-to-props'
 import styles from './index.module.css'
-import { Button, Icon } from 'antd'
+import { Button, Icon, Modal } from 'antd'
 import 'antd/dist/antd.css'
 
+const { confirm } = Modal
 class DeleteAllNotes extends React.Component {
+  deleteAll = () => {
+    const deleteAllFunc = this.props.onDeleteAllNotes
+    confirm({
+      title: 'Are you sure to delete all the notes?',
+      onOk() {
+        deleteAllFunc()
+      },
+      onCancel() {},
+    })
+  }
+
   render() {
     return (
       <div
         className={styles.DeleteAllNotes}
-        onClick={this.props.onDeleteAllNotes}>
+        onClick={this.deleteAll}>
         <Button ghost type="danger"><Icon type="delete" />Delete all</Button>
       </div>
     )
--- src/components/SiderBar/index.js
+++ src/components/SiderBar/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import styles from './index.module.css'
-import 'hamburgers/dist/hamburgers.css'
 import './index.css'
 import { connect } from "react-redux"
 import mapDispatchToProps from './map-dispatch-to-props'
@@ -8,17 +7,21 @@ import mapStateToProps from './map-state-to-props'
 import NotesList from '../NotesList'
 import AddNewNote from '../AddNewNote'
 import DeleteAllNotes from '../DeleteAllNotes'
+import { Icon } from 'antd'
+import 'antd/dist/antd.css'
 
 class Siderbar extends Component {
   render() {
+    const burgerStyle = {fontSize: 'x-large'}
+    if (this.props.isSidebarOpen) {
+      burgerStyle['color'] = 'white'
+    } else {
+      burgerStyle['color'] = 'grey'
+    }
     return (
       <div className={this.props.isSidebarOpen ? styles.SiderbarOpen : styles.Siderbar}>
         <div className={styles.Burger}>
-          <div className={this.props.isSidebarOpen ? styles.HamburgerOpen : styles.Hamburger} onClick={() => {this.props.onUpdateSidebarOpen()}} >
-            <div className="hamburger-box">
-              <div className={`hamburger-inner ${this.props.isSidebarOpen ? 'white-hamburger' :  ''}`}></div>
-            </div>
-          </div>
+          <Icon type="menu" style={burgerStyle} onClick={() => {this.props.onUpdateSidebarOpen()}} />
         </div>
         {this.props.isSidebarOpen ?
           <div className={styles.List}>
